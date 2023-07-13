d87ee2a0ccc8e07e0dc78f13394649d631ccbf10
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2023-07-13 07:37:37 +0200

    Release v1.0.2

--- src/main/java/de/keksuccino/justzoom/ZoomHandler.java
+++ src/main/java/de/keksuccino/justzoom/ZoomHandler.java
@@ -4,17 +4,23 @@ import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ZoomHandler {
 
-    private static Minecraft mc = Minecraft.getInstance();
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private static final Minecraft MC = Minecraft.getInstance();
 
     public static boolean isZoomed = false;
-    public static double zoomFactor = JustZoom.config.getOrDefault("base_zoom_factor", 0.25D);
+    public static double zoomFactor = JustZoom.getConfig().getOrDefault("base_zoom_factor", 0.25D);
+    public static double lerpAmount = JustZoom.getConfig().getOrDefault("lerp_amount", 0.1D);
 
     private static double cachedFov;
     private static double cachedDefaultFov;
     private static boolean cachedSmoothCamera;
+    private static boolean lastFov = false;
 
     public static void init() {
 
@@ -33,10 +39,12 @@ public class ZoomHandler {
 
         if (KeyBindings.keyToggleZoom.isDown()) {
 
-            if (!isZoomed) {
-                cachedSmoothCamera = mc.options.smoothCamera;
+            if (JustZoom.getConfig().getOrDefault("smooth_camera_on_zoom", true)) {
+                if (!isZoomed) {
+                    cachedSmoothCamera = MC.options.smoothCamera;
+                }
+                MC.options.smoothCamera = true;
             }
-            mc.options.smoothCamera = true;
             isZoomed = true;
 
             double modifiedZoom = fov * zoomFactor;
@@ -46,17 +54,35 @@ public class ZoomHandler {
             if (modifiedZoom > 170.0D) {
                 modifiedZoom = 170.0D;
             }
-            cachedFov = modifiedZoom;
-            return (modifiedZoom);
+
+            cachedFov = cachedFov + (modifiedZoom - cachedFov) * lerpAmount;
+            if (cachedFov > cachedDefaultFov) {
+                cachedFov = cachedDefaultFov;
+            }
+            return cachedFov;
 
         } else {
 
             if (isZoomed) {
-                mc.options.smoothCamera = cachedSmoothCamera;
-                isZoomed = false;
-                if (JustZoom.config.getOrDefault("reset_zoom_factor", true)) {
-                    zoomFactor = JustZoom.config.getOrDefault("base_zoom_factor", 0.25D);
+                if (!lastFov) {
+                    cachedFov = cachedFov + (cachedDefaultFov - cachedFov) * lerpAmount;
                 }
+                if ((Math.abs(cachedFov - cachedDefaultFov) < 1 || (cachedFov - cachedDefaultFov) >= 0) && !lastFov) {
+                    if (cachedFov < cachedDefaultFov) {
+                        cachedFov += 1;
+                    }
+                    lastFov = true;
+                }
+                if (lastFov) {
+                    cachedFov = cachedDefaultFov;
+                    if (JustZoom.getConfig().getOrDefault("smooth_camera_on_zoom", true)) MC.options.smoothCamera = cachedSmoothCamera;
+                    isZoomed = false;
+                    if (JustZoom.getConfig().getOrDefault("reset_zoom_factor", true)) {
+                        zoomFactor = JustZoom.getConfig().getOrDefault("base_zoom_factor", 0.25D);
+                    }
+                    lastFov = false;
+                }
+                return cachedFov;
             }
 
         }
@@ -71,19 +97,23 @@ public class ZoomHandler {
     public void onMouseScroll(InputEvent.MouseScrollingEvent e) {
 
         if (KeyBindings.keyToggleZoom.isDown()) {
+
             e.setCanceled(true);
+
             if (e.getScrollDelta() < 0) {
                 if (cachedFov < 170.0D) {
-                    if (JustZoom.config.getOrDefault("zoom_out_cap", true) && (cachedFov >= cachedDefaultFov)) {
+                    if (JustZoom.getConfig().getOrDefault("zoom_out_cap", true) && (cachedFov >= cachedDefaultFov)) {
                         return;
                     }
-                    zoomFactor += JustZoom.config.getOrDefault("zoom_out_per_scroll", 0.05D);
+                    zoomFactor += JustZoom.getConfig().getOrDefault("zoom_out_per_scroll", 0.05D);
                 }
             } else if (e.getScrollDelta() > 0) {
                 if (cachedFov > 1.0D) {
-                    zoomFactor -= JustZoom.config.getOrDefault("zoom_in_per_scroll", 0.05D);
+                    zoomFactor -= JustZoom.getConfig().getOrDefault("zoom_in_per_scroll", 0.05D);
+                    if (zoomFactor < 0) zoomFactor = 0;
                 }
             }
+
         }
 
     }
