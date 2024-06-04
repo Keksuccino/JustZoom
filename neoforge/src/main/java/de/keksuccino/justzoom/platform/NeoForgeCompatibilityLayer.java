5df5d7c20e3fc403c99ea13e276fd3910af73bc1
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2024-06-04 11:22:37 +0200

    ---

--- neoforge/src/main/java/de/keksuccino/justzoom/platform/NeoForgeCompatibilityLayer.java
+++
@@ -1,7 +0,0 @@
-package de.keksuccino.justzoom.platform;
-
-import de.keksuccino.justzoom.platform.services.IPlatformCompatibilityLayer;
-
-public class NeoForgeCompatibilityLayer implements IPlatformCompatibilityLayer {
-
-}
