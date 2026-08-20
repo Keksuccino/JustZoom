5f2ae2bc1a46e5aa63d522b7b89f3aab1900111a
Keksuccino <Keksuccino@users.noreply.github.com>
2026-08-20 09:33:30 +0200

    ---

--- common/src/main/java/de/keksuccino/justzoom/JustZoom.java
+++ common/src/main/java/de/keksuccino/justzoom/JustZoom.java
@@ -16,6 +16,7 @@ public class JustZoom {
     public static final String LOADER = Services.PLATFORM.getPlatformName().toUpperCase();
     public static final File MOD_DIR = createDirectory(new File(GameDirectoryUtils.getGameDirectory(), "/config/justzoom"));
     public static final File INSTANCE_DATA_DIR = createDirectory(new File(GameDirectoryUtils.getGameDirectory(), "/justzoom_instance_data"));
+    public static final File INSTANCE_PERSISTENCE_DATA = new File(INSTANCE_DATA_DIR, "/persistence_data.json");
 
     private static Options options;
 
